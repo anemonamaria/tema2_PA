e574853a20c95f88613cf5d50e039e8955bc4a03
Anemona Maria M <57135826+anemonamaria@users.noreply.github.com>
2021-06-01 01:18:05 +0300

    Add files via upload

--- adrese.cpp
+++ adrese.cpp
@@ -6,43 +6,13 @@ struct Person {
 	string name;
 	int number;
 	vector<string> email;
-	int total;
-	int printed;
 	vector<int> connections;
-	int connected;
 };
 
 struct Mail {
 	string mail;
-	string name;
-	string nr;
 	int position;
 };
-
-int person_sorter(Person *a, Person *b) {
-	if(a->name.compare(b->name) != 0)
-		return a->name.compare(b->name);
-	else if(a->number > b->number)
-		return 1;
-	else return -1;
-}
-
-int findPersoana(Person *pers, string mail) {
-	// for (int i = 0; i <  pers->number; i++) {
-	// 	if(pers->email == mail) return 1;
-	// }
-	// return 0;
-	return find(pers->email.begin(), pers->email.end(), mail) != pers->email.end();
-}
-
-void printMails( Person *pers) {
-	ofstream fout("adrese.out");
-	fout<<"anaaaa";
-	for (int i = 0 ; i < pers->number; i++) {
-		fout << pers->email[i] << "\n";
-	}
-	pers->printed = 1;
-}
 
 int main() {
 	ifstream fin("adrese.in");
@@ -52,7 +22,6 @@ int main() {
 	fin >> n;
 
 	string name_aux;
-	int number_aux;
 	string email_aux;
 
 	vector<Person *> persoane(n);
@@ -63,11 +32,6 @@ int main() {
 		Mail *mail_aux = new Mail;
 		fin >> pers_aux->name;
 		fin >> pers_aux->number;
-		pers_aux->total = pers_aux->number;
-		pers_aux->connected = 0;
-		pers_aux->printed = 0;
-		mail_aux->name = pers_aux->name;
-		mail_aux->nr = pers_aux->number;
 		mail_aux->position = i;
 		dim = dim + pers_aux->number;
 		for (int j = 0; j < pers_aux->number; j++) {
@@ -84,13 +48,18 @@ int main() {
 
 	for(int i = 0; i < n; i++) {
 		for (int j = 0; j < dim; j++) {
+			// cautam mail-ul
 			if (i != mails[j]->position &&
-				find(persoane[i]->connections.begin(), persoane[i]->connections.end(), mails[j]->position) ==  persoane[i]->connections.end() &&
-				find(persoane[i]->email.begin(), persoane[i]->email.end(), mails[j]->mail) != persoane[i]->email.end()) {
-					//persoane[i]->email.push_back(mails[j]->mail);
-					//persoane[i]->number = persoane[i]->number + persoane[mails[j]->position]->number ; // -1 ???
+				find(persoane[i]->connections.begin(), persoane[i]->
+					connections.end(), mails[j]->position) ==
+				 	persoane[i]->connections.end() &&
+				find(persoane[i]->email.begin(), persoane[i]->email.end(),
+					mails[j]->mail) != persoane[i]->email.end()) {
 				for (int k = persoane[mails[j]->position]->number - 1; k >= 0; k--) {
-					if(persoane[mails[j]->position]->email[k].compare(persoane[mails[j]->position]->email[k]) != 0) {
+					if (find(persoane[i]->email.begin(), persoane[i]->email
+						.end(), persoane[mails[j]->position]->email[k])
+						== persoane[i]->email.end()) {
+						// actualizam vectorul de persoane
 						persoane[i]->email.push_back(persoane[mails[j]->position]->email[k]);
 						persoane[i]->number = persoane[i]->number + 1;
 					}
@@ -98,72 +67,32 @@ int main() {
 				}
 				nr_rez--;
 
-				//fout << mails[j]->mail <<" " << i<<" " << mails[j]->position<<"\n";
 				persoane[mails[j]->position]->connections.push_back(i);
 				persoane[i]->connections.push_back(mails[j]->position);
 				persoane[mails[j]->position]->number = 0;
 				persoane[mails[j]->position]->name = "deleted";
-
-
 			}
 		}
 		sort(persoane[i]->email.begin(), persoane[i]->email.end());
 	}
 
-	for (int i = 0; i < n; i++) {
-		fout<< persoane[i]->name << "\n";
-		for (int j = 0; j < persoane[i]->number; j++) {
-			fout <<persoane[i]->email[j] << "\n";
+	fout << nr_rez << "\n";
+	sort(persoane.begin(), persoane.end(), [](Person *a, Person *b)
+		{if (a->number == b->number) {
+			return (a->name < b->name);
+		} else {
+			return (a->number < b->number);
+		}});
+
+	for (int i = 0; i < (int)persoane.size(); i++) {
+		if(persoane[i]->number == 0) {
+			continue;
 		}
-		fout <<" \n";
+
+		fout << persoane[i]->name << " " << persoane[i]->number << "\n";
+		for (int j = 0; j < (int)persoane[i]->email.size(); j++) {
+			fout << persoane[i]->email[j] << "\n";
+		}
 	}
-
-	fout << nr_rez;
-	// int total = n;
-	// string rez;
-	//vector<Person *> persoane_aux;
-//	sort(persoane.begin(), persoane.end(), &person_sorter);
-// 	for(int i = 0; i< n;i++) {
-// 		for(int j = 0; j< persoane[i]->number; j++) {
-// 			fout << persoane[i]->email[j] << "\n";
-// 		}
-// 		fout << "\n";
-// 	}
-// 	int aux = 0;
-// 	while (aux < n - 1) {
-// 		for(int j = 0; j < persoane[aux]->number; j++) {
-// 			int aux2 = aux + 1;
-
-// 			fout << findPersoana(persoane[aux2], persoane[aux]->email[j]) << "lalalal " << persoane[aux2]->name << " " <<  persoane[aux]->email[j] << " " << aux2 <<  "\n";
-// 			//fout << find(persoane[aux2]->email.begin(), persoane[aux2]->email.end(),persoane[aux]->email[j]) << "balababakbakabk\n";
-// 			if (findPersoana(persoane[aux2], persoane[aux]->email[j]) == 1) {
-// 				persoane[aux2]->total = persoane[aux2]->total + persoane[aux]->number;
-// 				persoane[aux]->total = persoane[aux]->total + persoane[aux2]->number;
-// 				persoane[aux2]->connected = persoane[aux]->connected + 1;
-// 				persoane[aux]->connected = persoane[aux2]->connected + 1;
-
-// 				persoane[aux2]->connections.push_back(aux);
-// 				persoane[aux]->connections.push_back(aux2);
-// 			}
-// 		}
-// 		aux++;
-// 	}
-
-// 	aux = 0;
-// 	while(aux < n) {
-// 		if (persoane[aux]->printed) {
-// 			aux ++;
-// 		} else {
-// 			fout << persoane[aux]->name << " " << persoane[aux]->total << "\n";
-
-// 			for(int j = 0; j < persoane[aux]->connected; j++) {
-// 			fout << persoane[aux]->name << " " << persoane[aux]->total << " " << persoane[aux]->connections[j] << "laa\n";
-
-// 				printMails(persoane[(persoane[aux]->connections[j])]);
-// 			}
-// 			aux++;
-// 		}
-// 	}
-// //	fout << persoane[1]->name;
 	return 0;
 }
--- poduri.cpp
+++ poduri.cpp
@@ -1,100 +1,81 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define ROW 9
-#define COL 10
 
-struct Point {
-    int x;
-    int y;
+struct Node {
+	int x;
+	int y;
+    int distanta;
 };
 
-struct queueNode {
-    Point pt;  // The cordinates of a cell
-    int dist;  // cell's distance of from the source
-};
-
-struct celula {
-	char tipnod;
-	int distanta;
-};
-
-bool isValid(int row, int col, int n, int m) {
-    return (row >= 0) && (row < n) &&
-           (col >= 0) && (col < m);
+bool isValid(int x, int y, int n, int m, vector <vector <char>> &matrix) {
+	// verific daca pozitia exista
+    if (x >= 0 && y < n && x >= 0 && y < m && matrix[x][y] != '.')
+		return 1;
+	return 0;
 }
 
-int rowD[] = {-1, 0, 0, 1};
-int colD[] = {0, -1, 1, 0};
-
-int rowO[] = {0, 0};
-int colO[] = {-1, 1};
-
-int rowV[] = {-1, 1};
-int colV[] = {0, 0};
-
-int BFS(ofstream &fout, vector <vector <char>> &mat, Point src, int n, int m) {
-    if (!isValid(src.x, src.y, n, m))
+int BFS(vector <vector <char>> &matrix, int x, int y, int n, int m) {
+	// parcurgerea matricii
+    if (!isValid(x, y, n, m, matrix))
         return -1;
 
-    bool visited[n][m];
-    memset(visited, false, sizeof visited);
+    bool vizitat[n][m];
+    memset(vizitat, false, sizeof(vizitat));
+    vizitat[x][y] = true;
 
-    visited[src.x][src.y] = true;
+    queue<Node> q;
+    q.push({x, y, 0});
 
-    queue<queueNode> q;
+    while (q.size()) {
+        Node nod = q.front();
+		q.pop();
 
-    queueNode s = {src, 0};
-    q.push(s);
-    while (!q.empty()) {
-        queueNode curr = q.front();
-        Point pt = curr.pt;
+		if ((nod.x == 0 && matrix[nod.x][nod.y] != 'O') ||
+			(nod.y == 0 && matrix[nod.x][nod.y] != 'V') ||
+			(nod.x == n-1 && matrix[nod.x][nod.y] != 'O') ||
+			(nod.y == m-1 && matrix[nod.x][nod.y] != 'V'))
+            return nod.distanta + 1;
 
-        if ((pt.x == 0 && mat[pt.x][pt.y] != 'O') ||
-			(pt.y == 0 && mat[pt.x][pt.y] != 'V') ||
-			(pt.x == n-1 && mat[pt.x][pt.y] != 'O') ||
-			(pt.y == m-1 && mat[pt.x][pt.y] != 'V'))
-            return curr.dist + 1;
-
-        q.pop();
-		if (mat[pt.x][pt.y] == 'D') {
-			for (int i = 0; i < 4; i++) {
-				int row = pt.x + rowD[i];
-				int col = pt.y + colD[i];
-
-				if (isValid(row, col, n, m) && mat[row][col] != '.' &&
-				!visited[row][col]) {
-					visited[row][col] = true;
-					queueNode Adjcell = { {row, col},
-										curr.dist + 1 };
-					q.push(Adjcell);
-				}
+		if (matrix[nod.x][nod.y] == 'D') {
+			if (isValid(nod.x - 1, nod.y, n, m, matrix) && !vizitat[nod.x - 1][nod.y]) {
+				vizitat[nod.x - 1][nod.y] = true;
+				q.push({nod.x - 1, nod.y, nod.distanta + 1});
 			}
-		} else if (mat[pt.x][pt.y] == 'O') {
-			for (int i = 0; i < 2; i++) {
-				int row = pt.x + rowO[i];
-				int col = pt.y + colO[i];
 
-				if (isValid(row, col, n, m) && mat[row][col] != '.' &&
-				!visited[row][col]) {
-					visited[row][col] = true;
-					queueNode Adjcell = { {row, col},
-										curr.dist + 1 };
-					q.push(Adjcell);
-				}
+			if (isValid(nod.x, nod.y - 1, n, m, matrix) && !vizitat[nod.x][nod.y - 1]) {
+				vizitat[nod.x][nod.y - 1] = true;
+				q.push({nod.x, nod.y - 1, nod.distanta + 1});
 			}
-		} else if (mat[pt.x][pt.y] == 'V') {
-			for (int i = 0; i < 2; i++) {
-				int row = pt.x + rowV[i];
-				int col = pt.y + colV[i];
 
-				if (isValid(row, col, n, m) && mat[row][col] != '.' &&
-				!visited[row][col]) {
-					visited[row][col] = true;
-					queueNode Adjcell = { {row, col},
-										curr.dist + 1 };
-					q.push(Adjcell);
-				}
+			if (isValid(nod.x, nod.y + 1, n, m, matrix) && !vizitat[nod.x][nod.y + 1]) {
+				vizitat[nod.x][nod.y + 1] = true;
+				q.push({nod.x, nod.y + 1, nod.distanta + 1});
+			}
+
+			if (isValid(nod.x + 1, nod.y, n, m, matrix) && !vizitat[nod.x + 1][nod.y]) {
+				vizitat[nod.x + 1][nod.y] = true;
+				q.push({nod.x + 1, nod.y, nod.distanta + 1});
+			}
+		} else if (matrix[nod.x][nod.y] == 'O') {
+			if (isValid(nod.x, nod.y - 1, n, m, matrix) && !vizitat[nod.x][nod.y - 1]) {
+				vizitat[nod.x][nod.y - 1] = true;
+				q.push({nod.x, nod.y - 1, nod.distanta + 1});
+			}
+
+			if (isValid(nod.x, nod.y + 1, n, m, matrix) && !vizitat[nod.x][nod.y + 1]) {
+				vizitat[nod.x][nod.y + 1] = true;
+				q.push({nod.x, nod.y + 1, nod.distanta + 1});
+			}
+		} else if (matrix[nod.x][nod.y] == 'V') {
+			if (isValid(nod.x - 1, nod.y, n, m, matrix) && !vizitat[nod.x - 1][nod.y]) {
+				vizitat[nod.x - 1][nod.y] = true;
+				q.push({nod.x - 1, nod.y, nod.distanta + 1});
+			}
+
+			if (isValid(nod.x + 1, nod.y, n, m, matrix) && !vizitat[nod.x + 1][nod.y]) {
+				vizitat[nod.x + 1][nod.y] = true;
+				q.push({nod.x + 1, nod.y, nod.distanta + 1});
 			}
 		}
     }
@@ -105,13 +86,10 @@ int BFS(ofstream &fout, vector <vector <char>> &mat, Point src, int n, int m) {
 int main() {
 	ifstream fin("poduri.in");
 	ofstream fout("poduri.out");
-	int n, m;
-	Point sursa;
-    fin >> n;
-	fin >> m;
-	fin >> sursa.x >> sursa.y;
-	sursa.x = sursa.x - 1;
-	sursa.y = sursa.y - 1;
+	int n, m, x, y;
+    fin >> n >> m;
+	fin >> x >> y;
+
 	vector <vector <char>> matrix(n);
 
 	for(int i = 0; i < n; i++) {
@@ -121,8 +99,7 @@ int main() {
 		}
 	}
 
-    int dist = BFS(fout, matrix, sursa, n, m);
-    fout << dist;
+    fout << BFS(matrix, x - 1, y - 1, n, m);
 
     return 0;
 }
